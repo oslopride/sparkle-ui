5eaed6d601df008c17bb023e2eddbba5e9a3d711
Siddise Hirpa <siddisehirpa@gmail.com>
2020-03-01 10:48:26 +0100

    Create new setup and test publish workflow

--- src/components/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC, ButtonHTMLAttributes } from 'react';
-
-const Button: FC<ButtonHTMLAttributes<HTMLButtonElement>> = props => {
-  return <button data-testid="button" {...props} />;
-};
-
-export default Button;
